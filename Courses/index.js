e58d95698db8ddd683f6afd421ac15af7541cd19
matchallenges <matchallenges04@gmail.com>
2021-04-16 13:07:31 -0400

    Retructure

---
+++ Courses/index.js
@@ -0,0 +1,29 @@
+var number = 5; //in-line comment
+
+/* 
+
+this is a multi-line comment
+
+*/
+
+number = 9;
+
+// Data Types: undefined, null, boolean, string, symbol, number and object
+
+var myName = "Mathieu" // used throughout the whole program
+
+myName = 8
+
+let ourName = "freecodecamp" // used throughout the scope of where you define it
+
+const pi = 3.14 // the variable never changes
+
+var a; // declaring
+var b = 2; // declaring and assigning in one line
+
+console.log(a)
+
+a = 7;
+b = a;
+
+console.log(a)
---
+++ Courses/playground1.js
@@ -0,0 +1,107 @@
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a string";
+
+// Do not change code below this line
+
+a += 1;
+b += 5;
+c = c +" String!";
+
+// Math
+
+var sum = 10 + 10;
+console.log(sum);
+
+var difference = 45 - 33;
+console.log(difference);
+
+// regular operators apply + - * /
+
+// Increment 
+
+var myVar = 1;
+myVar++;
+myVar--;
+
+var myDecimal = 0.008;
+
+var remainder;
+remainder = 11 % 3;
+
+myVar *= 10;
+console.log(myVar);
+
+myVar /= 10;
+console.log(myVar);
+
+// Escape characters \
+
+var myStr = "\"Mathieu\"";
+console.log(myStr);
+
+var myStr2 = '"Mathieu"';
+console.log(myStr2);
+
+var myStr3 = `'"Woah single and double quotes, how did he do that?"'`;
+console.log(myStr3);
+
+// Concatenate strings
+
+var ourStr = "I come first. " + "I come second.";
+
+var ourStr2 = "I come third. ";
+ourStr2 += "I come fourth.";
+
+// Length of a string
+
+console.log(ourStr.length)
+
+// Indexing
+
+console.log(ourStr[0])
+console.log(ourStr[ourStr.length - ourStr.length + 1])
+
+// The characters of strings are immutable
+
+// Arrays
+
+var ourArray = ["John", 23, [1, 2, 3]]
+
+console.log(ourArray[2])
+
+// Array indexes are mutable
+
+ourArray[0] = `Mathieu`
+
+console.log(ourArray[0])
+
+// Append data to the end of an array with push ourArray.push()
+
+ourArray.push('Jackson', 'Charles', 'Fargo')
+
+console.log(ourArray)
+
+// Remove last variable using ourArray.pop()
+
+ourArray.pop()
+
+ourArray.shift()
+console.log(ourArray) // shift to the left
+ourArray.unshift("Lucas") // adds Lucas to the start of the array 
+
+// Functions
+
+function adding(x, y){
+    sum = x + y
+    return sum
+}
+
+adding(3, 4)
+
+var collections = {
+    "poison" : "cyanide"
+}
+
+console.log(collections.poison)
