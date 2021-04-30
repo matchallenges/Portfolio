cadd06e40a9d02c7c71c8f92a2823b742de74f3c
matchallenges <matchallenges04@gmail.com>
2021-04-30 14:35:02 -0400

    Arrays from ANSI C do not work fro some reason

--- 2021/x86Learning/firstFile.asm
+++ 2021/x86Learning/firstFile.asm
@@ -1,10 +1,11 @@
-global  _main
+	global  _main
     extern  _printf
-     section .text
-  _main:
-        push    message
-        call    _printf
-        add     esp, 4
-        ret
-   message:
-        db      'Hello, World', 10, 0
+	section .text
+ _main:
+  	push    message
+   	call    _printf
+   	add     esp, 4
+    ret
+ message:
+ 	db      'Hello, World', 10, 0
+	END
