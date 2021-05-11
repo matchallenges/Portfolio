acdf7d1106e69ff3c5f92f11486e80b00329b59b
matchallenges <matchallenges04@gmail.com>
2021-05-11 13:01:25 -0400

    intro.py added

---
+++ 2021/PythonProjects/intro.py
@@ -0,0 +1,17 @@
+#for loop
+for i in range(10):
+    print("Wow")
+'''
+multi-line
+
+
+comment
+
+'''
+
+country = "Canada"
+
+if(country == "Canada"):
+    print("Hello, Canada")
+else:
+    print("Hello, America")
