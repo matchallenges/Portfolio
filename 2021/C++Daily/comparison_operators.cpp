fd7e443668ff42967413f1573cd811c650565d1a
matchallenges <matchallenges04@gmail.com>
2021-06-24 15:15:38 -0400

    Re-formatting

---
+++ 2021/C++Daily/comparison_operators.cpp
@@ -0,0 +1,10 @@
+/* if (boolean-expression) -> statement
+
+if (boolean-expression-1) statement-1
+else if (boolean-expression-2) statement-2
+else statement-3
+
+return -> evaluted variable
+
+*/
+
