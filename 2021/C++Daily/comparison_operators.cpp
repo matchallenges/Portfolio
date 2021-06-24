d4ec580998fc08aa6087861b97e92f6596d1a82a
matchallenges <matchallenges04@gmail.com>
2021-06-24 15:23:53 -0400

    Boolean expressions

--- 2021/C++Daily/comparison_operators.cpp
+++ 2021/C++Daily/comparison_operators.cpp
@@ -8,3 +8,11 @@ return -> evaluted variable
 
 */
 
+#include <cstdio>
+
+int main(){
+    int x = 0;
+    if (x > 0) printf ("Positive.");
+    else if (x < 0) printf ("Negative.");
+    else printf("Zero.");
+}
--- 2021/CDaily/main1.c
+++ 2021/CDaily/main1.c
@@ -1,26 +1,2 @@
 #include <stdio.h>
 
-/*  */
-
-/* print the Frenheit-Celsius conversion table
-    for fahr = 0, 20, ..., 300 */ 
-
-int main()
-{
-    /* code goes here */
-
-    int fahr, celsius; /* initialize the temperature integers */
-    int lower, upper, step; /* initialize the flow control integers */
-
-    lower = 0; /* lower limit of temperature size */
-    upper = 300; /* upper limit */
-    step = 20; /* step size (intervals) */
-
-    fahr = lower; /* assign fahr to the integer lower */
-    while (fahr <= upper) { /* while fahr is less than or equal to upper */
-        celsius = 5 * (fahr-32) / 9; /* each loop celsius will be converted from the fahr variable */
-        printf("%d\t%d\n", fahr, celsius); /* print formatting where %d specifies an integer argument, with the arguments proceeding */ 
-        fahr = fahr + step; /* increments fahr by the step */ 
-   } 
-
-}
