d8c13ee459219494aed3353a7013b81d9681c520
matchallenges <matchallenges04@gmail.com>
2021-06-27 15:45:23 -0400

    Handling decay

--- 2021/C++Daily/Handling_decay_in_arrays.cpp
+++ 2021/C++Daily/Handling_decay_in_arrays.cpp
@@ -1,8 +1,17 @@
 #include <cstdio>
 #include <iostream>
 
-struct Colledge{
+struct College{
     char name[256];
 };
 
-int main(){}
+void print_names(College* colleges, size_t n_colleges){
+    for (size_t i = 0; i < n_colleges; i++){
+        printf("%s College\n", colleges[i]);
+    }
+}
+
+int main(){
+    College oxford[] = {"Magdalen", "Nuffield", "Kellog"};
+    print_names(oxford, sizeof(oxford) / sizeof(College));
+}
